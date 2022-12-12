f954fa122030b24805670fc13284e74bad6d0cf7
Timothy Mullins <34033344+Tmullins003@users.noreply.github.com>
2022-12-11 23:19:16 -0500

    Second Upload
    Read to an excel file called Donors.  set the file to a data frame using pandas.  then using sum() added up recurring and one-time gifts to give the end-user their yearly salary which is then broken down into monthly bi-weekly and weekly sums.

--- Donors.py
+++ Donors.py
@@ -1,7 +1,7 @@
 """
 Goal: create a program where the end-user can continuously append data in an Excel sheet. In this case we will be
-recreating a donorhub for Vagabond Missions. Where the missionaries or Development Officers can insert new donor
-information. It will also do the following functions
+recreating a donor-hub for Vagabond Missions. Where the missionaries or Development Officers can dp the following
+functions:
     1. Show Fund % to end-user goal
     2. Fund life expectancy(when will I hit 0)
     3. Show and Separate One Time Gifts and Recurring Gifts
@@ -10,8 +10,8 @@ information. It will also do the following functions
     6. be able to read and write donors info
 
 """
-import pandas as pd
 
+'''
 df = pd.DataFrame(columns=["Donor ID", "Donor Name", "Amount Donated"])
 donorInput = int(input("Enter how many NEW donors: "))
 
@@ -26,3 +26,21 @@ df.index = range(len(df.index))
 
 print(df)
 df.to_excel(r'D:\CodingProjects\DonorProject\Donors.xlsx', sheet_name='Sheet 1', index=False)  # writes to Excel file
+this could be used to write to a new sheet and then combine to the old sheet therefore creating a continuous updated sheet
+'''
+# Right now were going to focus on just reading from a given xlsx then worry about appending it from a user later
+
+import pandas as pd
+
+donor_file = pd.read_excel('D:\CodingProjects\DonorProject\Donors.xlsx', sheet_name='RCG', index_col='Donor ID')
+donor_file2 = pd.read_excel('D:\CodingProjects\DonorProject\Donors.xlsx', sheet_name='OTG', index_col='Donor ID')
+df = pd.DataFrame(donor_file)
+df2 = pd.DataFrame(donor_file2)
+print(df,'\n\n', df2)
+t1 = df['Gift Amount'].sum()
+t2 = df2['Gift Amount'].sum()
+fundTotal = (df['Gift Amount'].sum() * 12) + df2['Gift Amount'].sum()
+print('Yearly Sum:', fundTotal)
+print('Monthly Sum:', fundTotal / 12)
+print('Bi-Weekly Sum:', fundTotal / 26)
+print('Weekly Sum:', fundTotal / 52)
